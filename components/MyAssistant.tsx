afe87e6ec47ca289fe39567a7df04e80d8117144
theic <ic.icreator@gmail.com>
2024-11-04 16:06:10 +0200

    Initial commit from Create Next App

---
+++ components/MyAssistant.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useRef } from "react";
+import { Thread } from "@assistant-ui/react";
+import { useLangGraphRuntime } from "@assistant-ui/react-langgraph";
+import { makeMarkdownText } from "@assistant-ui/react-markdown";
+
+import { createThread, getThreadState, sendMessage } from "@/lib/chatApi";
+
+const MarkdownText = makeMarkdownText();
+
+export function MyAssistant() {
+  const threadIdRef = useRef<string | undefined>();
+  const runtime = useLangGraphRuntime({
+    threadId: threadIdRef.current,
+    stream: async (messages) => {
+      if (!threadIdRef.current) {
+        const { thread_id } = await createThread();
+        threadIdRef.current = thread_id;
+      }
+      const threadId = threadIdRef.current;
+      return sendMessage({
+        threadId,
+        messages,
+      });
+    },
+    onSwitchToNewThread: async () => {
+      const { thread_id } = await createThread();
+      threadIdRef.current = thread_id;
+    },
+    onSwitchToThread: async (threadId) => {
+      const state = await getThreadState(threadId);
+      threadIdRef.current = threadId;
+      return { messages: state.values.messages };
+    },
+  });
+
+  return (
+    <Thread
+      runtime={runtime}
+      assistantMessage={{ components: { Text: MarkdownText } }}
+    />
+  );
+}
---
+++ lib/chatApi.ts
@@ -0,0 +1,40 @@
+import { Client, ThreadState } from "@langchain/langgraph-sdk";
+import { LangChainMessage } from "@assistant-ui/react-langgraph";
+
+const createClient = () => {
+  const apiUrl =
+    process.env["NEXT_PUBLIC_LANGGRAPH_API_URL"] ||
+    new URL("/api", window.location.href).href;
+  return new Client({
+    apiUrl,
+  });
+};
+
+export const createThread = async () => {
+  const client = createClient();
+  return client.threads.create();
+};
+
+export const getThreadState = async (
+  threadId: string
+): Promise<ThreadState<{ messages: LangChainMessage[] }>> => {
+  const client = createClient();
+  return client.threads.getState(threadId);
+};
+
+export const sendMessage = async (params: {
+  threadId: string;
+  messages: LangChainMessage[];
+}) => {
+  const client = createClient();
+  return client.runs.stream(
+    params.threadId,
+    process.env["NEXT_PUBLIC_LANGGRAPH_ASSISTANT_ID"]!,
+    {
+      input: {
+        messages: params.messages,
+      },
+      streamMode: "messages",
+    }
+  );
+};
